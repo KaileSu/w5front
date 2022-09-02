1c3668f2017b5dae44e3a4c824fc9ba3116f3c06
KaileSu <kailepku@gmail.com>
2022-09-03 08:19:56 +1000

    3sep

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+};
+const BACKEND_URL = 'https://s2179956.elf.ict.griffith.edu.au:3001';
+// for angular http methods
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  email = '';
+  password = '';
+  
+  constructor(private router:Router, private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+  submit(){
+    
+    let user = {username:this.email, pwd: this.password};
+ this.httpClient.post(BACKEND_URL + '/login', user,  httpOptions)
+// this.httpClient.post(BACKEND_URL + '/login', user)
+    .subscribe((data:any)=>{
+      alert("posting: " +JSON.stringify(user));
+
+      alert("postRes: " +JSON.stringify(data));
+
+      if (data.ok){
+        alert("correct");
+        sessionStorage.setItem('userid', data.userid.toString());
+        sessionStorage.setItem('userlogin', data.ok.toString());
+        sessionStorage.setItem('username', data.username);
+        sessionStorage.setItem('userbirthdate', data.userbirthdate);
+        sessionStorage.setItem('userage', data.userage.toString());
+
+        this.router.navigateByUrl("/account");
+      }
+      else { alert("email or password incorrect");}
+
+
+    })
+
+
+
+    
+  }
+
+}
